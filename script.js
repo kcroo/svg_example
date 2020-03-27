0a65c8949718f88c1e0e4f884d40f9bc72096cd8
Kirsten Corrao <kirstencorrao@gmail.com>
2020-03-26 17:07:19 -0700

    description appears near cursor

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ description = document.getElementById("description");
 district1.addEventListener("mouseenter", event => {
     description.classList.remove("inactive");
     description.classList.add("active");
+    xCoord = event.clientX + "px";
+    yCoord = event.clientY + "px";
+    description.style.left = xCoord;
+    description.style.top = yCoord;
 });
 
 district1.addEventListener("mouseleave", event => {
