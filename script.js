c35c2dd858f23c01c7466d6f0f9415ddf77b547e
Kirsten Corrao <kirstencorrao@gmail.com>
2020-03-27 12:47:26 -0700

    shows boundaries with different styles

--- script.js
+++ script.js
@@ -4,52 +4,98 @@ https://github.com/WebsiteBeaver/interactive-and-responsive-svg-map-of-us-states
 https://codepen.io/websitebeaver/pen/oLGGNz
 */
 
-map = document.getElementById("oregon_map");
-district1 = document.getElementById("district1");
-district2 = document.getElementById("district2");
-district3 = document.getElementById("district3");
-district4 = document.getElementById("district4");
-district5 = document.getElementById("district5");
-description = document.getElementById("description");
+// var map = new L.Map("mapid", {
+//     center: new L.LatLng(44.1555966, -120.6847490),
+//     zoom: 9,
+// });
 
-let district_descriptions = {
-    "district1": "District 1",
-    "district2": "District 2",
-    "district3": "District 3",
-    "district4": "District 4",
-    "district5": "District 5"
+// L.tileLayer('/nyt_covid19.png', {
+// }).addTo(map);
+
+// var congressionalDistricts = fetch("Congressional_Districts.geojson")
+// .then(function(response) {
+//   return response.json();
+// });
+
+var map = L.map('mapid').setView([44.1555966, -120.6847490], 7);
+        // mapLink = 
+        //     '<a href="http://openstreetmap.org">OpenStreetMap</a>';
+        // L.tileLayer(
+        //     'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        //     attribution: '&copy; ' + mapLink + ' Contributors',
+        //     maxZoom: 18,
+        //     }).addTo(map);
+
+// var svgElement = document.createElementNS("oregon_counties.svg", "svg");
+// var bounds = [[46, -124], [42, -116]];
+// L.svgOverlay(svgElement, bounds).addTo(map);
+
+
+var districtStyle = {
+    fillColor: "#FF0000",
+    fillOpacity: 1,
+    color: "FFFFFF",
+    weight: 2
 };
 
-function addDescriptionBox(description, event) {
-    description.classList.remove("inactive");
-    description.classList.add("active");
-    xCoord = event.clientX + "px";
-    yCoord = event.clientY + "px";
-    description.style.left = xCoord;
-    description.style.top = yCoord;
-}
+var congressionalDistricts = new L.GeoJSON.AJAX("/Congressional_Districts.geojson", {style: districtStyle});
+congressionalDistricts.addTo(map);
 
-district1.addEventListener("click", event => {
-    addDescriptionBox(description, event);
-    description.innerHTML = district_descriptions["district1"];
-});
+var countyStyle = {
+    fillOpacity: 0,
+    color: "#000000",
+    weight: 1
+};
 
-district2.addEventListener("click", event => {
-    addDescriptionBox(description, event);
-    description.innerHTML = district_descriptions["district2"];
-});
+var oregonCounties = new L.GeoJSON.AJAX("/oregon_counties.geojson", {style: countyStyle});       
+oregonCounties.addTo(map);
 
-district3.addEventListener("click", event => {
-    addDescriptionBox(description, event);
-    description.innerHTML = district_descriptions["district3"];
-});
+// map = document.getElementById("oregon_map");
+// district1 = document.getElementById("district1");
+// district2 = document.getElementById("district2");
+// district3 = document.getElementById("district3");
+// district4 = document.getElementById("district4");
+// district5 = document.getElementById("district5");
+// description = document.getElementById("description");
 
-district4.addEventListener("click", event => {
-    addDescriptionBox(description, event);
-    description.innerHTML = district_descriptions["district4"];
-});
+// let district_descriptions = {
+//     "district1": "District 1",
+//     "district2": "District 2",
+//     "district3": "District 3",
+//     "district4": "District 4",
+//     "district5": "District 5"
+// };
 
-district5.addEventListener("click", event => {
-    addDescriptionBox(description, event);
-    description.innerHTML = district_descriptions["district5"];
-});
+// function addDescriptionBox(description, event) {
+//     description.classList.remove("inactive");
+//     description.classList.add("active");
+//     xCoord = event.clientX + "px";
+//     yCoord = event.clientY + "px";
+//     description.style.left = xCoord;
+//     description.style.top = yCoord;
+// }
+
+// district1.addEventListener("click", event => {
+//     addDescriptionBox(description, event);
+//     description.innerHTML = district_descriptions["district1"];
+// });
+
+// district2.addEventListener("click", event => {
+//     addDescriptionBox(description, event);
+//     description.innerHTML = district_descriptions["district2"];
+// });
+
+// district3.addEventListener("click", event => {
+//     addDescriptionBox(description, event);
+//     description.innerHTML = district_descriptions["district3"];
+// });
+
+// district4.addEventListener("click", event => {
+//     addDescriptionBox(description, event);
+//     description.innerHTML = district_descriptions["district4"];
+// });
+
+// district5.addEventListener("click", event => {
+//     addDescriptionBox(description, event);
+//     description.innerHTML = district_descriptions["district5"];
+// });
