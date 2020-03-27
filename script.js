2974f5c3ea698fb1eee6bcc9dc484cd4ca76d6d1
Kirsten Corrao <kirstencorrao@gmail.com>
2020-03-26 17:18:31 -0700

    dictionary for descriptions; works for 2 districts

--- script.js
+++ script.js
@@ -6,7 +6,16 @@ https://codepen.io/websitebeaver/pen/oLGGNz
 
 map = document.getElementById("oregon_map");
 district1 = document.getElementById("district_1");
+district2 = document.getElementById("district_2");
 description = document.getElementById("description");
+
+let district_descriptions = {
+    "district1": "District 1",
+    "district2": "District 2",
+    "district3": "District 3",
+    "district4": "District 4",
+    "district5": "District 5"
+};
 
 district1.addEventListener("mouseenter", event => {
     description.classList.remove("inactive");
@@ -15,9 +24,25 @@ district1.addEventListener("mouseenter", event => {
     yCoord = event.clientY + "px";
     description.style.left = xCoord;
     description.style.top = yCoord;
+    description.innerHTML = district_descriptions["district1"];
 });
 
 district1.addEventListener("mouseleave", event => {
+    description.classList.remove("active");
+    description.classList.add("inactive");
+});
+
+district2.addEventListener("mouseenter", event => {
+    description.classList.remove("inactive");
+    description.classList.add("active");
+    xCoord = event.clientX + "px";
+    yCoord = event.clientY + "px";
+    description.style.left = xCoord;
+    description.style.top = yCoord;
+    description.innerHTML = district_descriptions["district2"];
+});
+
+district2.addEventListener("mouseleave", event => {
     description.classList.remove("active");
     description.classList.add("inactive");
 });
