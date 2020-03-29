c224cc2cd365346c47df5d694b3a3c5423e363ae
Kirsten Corrao <kirstencorrao@gmail.com>
2020-03-29 12:43:14 -0700

    removed old code; added comments

--- script.js
+++ script.js
@@ -1,40 +1,7 @@
-/* Sources
-https://codepen.io/brianplaza/pen/DfKsx
-https://github.com/WebsiteBeaver/interactive-and-responsive-svg-map-of-us-states-capitals
-https://codepen.io/websitebeaver/pen/oLGGNz
-*/
-
-// var map = new L.Map("mapid", {
-//     center: new L.LatLng(44.1555966, -120.6847490),
-//     zoom: 9,
-// });
-
-// L.tileLayer('/nyt_covid19.png', {
-// }).addTo(map);
-
-// var congressionalDistricts = fetch("Congressional_Districts.geojson")
-// .then(function(response) {
-//   return response.json();
-// });
-
+// create leaflet map
 var map = L.map('mapid').setView([44.1555966, -120.6847490], 7);
-        // mapLink = 
-        //     '<a href="http://openstreetmap.org">OpenStreetMap</a>';
-        // L.tileLayer(
-        //     'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        //     attribution: '&copy; ' + mapLink + ' Contributors',
-        //     maxZoom: 18,
-        //     }).addTo(map);
 
-// var svgElement = document.createElementNS("oregon_counties.svg", "svg");
-// var bounds = [[46, -124], [42, -116]];
-// L.svgOverlay(svgElement, bounds).addTo(map);
-
-/* styling 
-"NDISTRICT":1
-"Party":"Democratic Party"
-*/
-
+// districts display blue for democrat, red for republican
 function getFillColor(feature) {
     if(feature.properties.Party === "Democratic Party") {
         return "#0000FF";
@@ -44,6 +11,7 @@ function getFillColor(feature) {
     }
 }
 
+// style for congressional districts; calls getFillColor for color based on party
 function defineStyle(feature) {
     return {
         fillColor: getFillColor(feature),
@@ -54,71 +22,24 @@ function defineStyle(feature) {
     }
 }
 
+// displays popup with congress member and party, which are in geojson properties
 function onEachFeature(feature, layer) {
     let text = feature.properties.Name + ": " + feature.properties.Party;
     layer.bindPopup(text, keepInView=true);
 }
 
-
+// make congressional district layer and add to map
 var congressionalDistricts = new L.GeoJSON.AJAX("/Congressional_Districts.geojson", {style: defineStyle, onEachFeature: onEachFeature});
 congressionalDistricts.addTo(map);
 
+// style for counties: no fill, thin black outline
 var countyStyle = {
     fillOpacity: 0,
     color: "#000000",
     weight: 1
 };
 
+// make county layer and add to map
 // interactive set to false allows popups from lower layer to display
 var oregonCounties = new L.GeoJSON.AJAX("/oregon_counties.geojson", {style: countyStyle, interactive: false});       
 oregonCounties.addTo(map);
-
-// map = document.getElementById("oregon_map");
-// district1 = document.getElementById("district1");
-// district2 = document.getElementById("district2");
-// district3 = document.getElementById("district3");
-// district4 = document.getElementById("district4");
-// district5 = document.getElementById("district5");
-// description = document.getElementById("description");
-
-// let district_descriptions = {
-//     "district1": "District 1",
-//     "district2": "District 2",
-//     "district3": "District 3",
-//     "district4": "District 4",
-//     "district5": "District 5"
-// };
-
-// function addDescriptionBox(description, event) {
-//     description.classList.remove("inactive");
-//     description.classList.add("active");
-//     xCoord = event.clientX + "px";
-//     yCoord = event.clientY + "px";
-//     description.style.left = xCoord;
-//     description.style.top = yCoord;
-// }
-
-// district1.addEventListener("click", event => {
-//     addDescriptionBox(description, event);
-//     description.innerHTML = district_descriptions["district1"];
-// });
-
-// district2.addEventListener("click", event => {
-//     addDescriptionBox(description, event);
-//     description.innerHTML = district_descriptions["district2"];
-// });
-
-// district3.addEventListener("click", event => {
-//     addDescriptionBox(description, event);
-//     description.innerHTML = district_descriptions["district3"];
-// });
-
-// district4.addEventListener("click", event => {
-//     addDescriptionBox(description, event);
-//     description.innerHTML = district_descriptions["district4"];
-// });
-
-// district5.addEventListener("click", event => {
-//     addDescriptionBox(description, event);
-//     description.innerHTML = district_descriptions["district5"];
-// });
