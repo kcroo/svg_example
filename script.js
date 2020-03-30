d6d75599460b74b4a8b85e70b0635b3779bd24ab
Kirsten Corrao <kirstencorrao@gmail.com>
2020-03-30 10:12:21 -0700

    added legend

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 // create leaflet map
 var map = L.map('mapid').setView([44.1555966, -120.6847490], 7);
 
+const mapColors = {
+    "Congressional Districts Lines": "#000000",
+    "Democratic Party": "#0000FF",
+    "Republican Party": "#FF0000",
+    "County Lines": "#000000"
+};
+
 // districts display blue for democrat, red for republican
 function getFillColor(feature) {
     if(feature.properties.Party === "Democratic Party") {
-        return "#0000FF";
+        return mapColors["Democratic Party"];
     }
     else if (feature.properties.Party === "Republican Party") {
-        return "#FF0000";
+        return mapColors["Republican Party"];
     }
 }
 
@@ -18,7 +25,7 @@ function defineStyle(feature) {
         fillOpacity: 1,
         weight: 6,
         opacity: 1,
-        color: "#FFFFFF"
+        color: mapColors["Congressional Districts Lines"]
     }
 }
 
@@ -35,11 +42,43 @@ congressionalDistricts.addTo(map);
 // style for counties: no fill, thin black outline
 var countyStyle = {
     fillOpacity: 0,
-    color: "#000000",
-    weight: 1
+    color: mapColors["County Lines"],
+    weight: 0.4
 };
 
 // make county layer and add to map
 // interactive set to false allows popups from lower layer to display
 var oregonCounties = new L.GeoJSON.AJAX("/oregon_counties.geojson", {style: countyStyle, interactive: false});       
 oregonCounties.addTo(map);
+
+// add legend 
+const legend = L.control({position: 'bottomright'});
+
+legend.onAdd = (map) => {
+    // creates div with classes info and legend 
+    const div = L.DomUtil.create('div', 'info legend');
+    div.innerHTML += '<h3>Legend</h3>';
+
+    // loop through our density intervals and generate a label with a colored square for each interval
+    for(let key in mapColors) {
+        console.log(key);
+        console.log(mapColors[key]);
+        if (key === "Congressional Districts Lines") {
+            div.innerHTML +=
+            '<span style="color:' + mapColors[key] + '"><b>&mdash;</b></span>' + key + '<br>';
+        }
+        else if (key === "County Lines") {
+            div.innerHTML +=
+            '<span style="color:' + mapColors[key] + '">&mdash;</span>' + key + '<br>';
+        }
+        else {
+            div.innerHTML +=
+            '<i style="background:' + mapColors[key] + '"></i>' + key + '<br>';
+        }
+        
+    }
+
+    return div;
+};
+
+legend.addTo(map);
